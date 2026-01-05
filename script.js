3aa00c7dda8eb021183ced2c4fdb11b07995b5a1
JezzerK <jeremykalfus@gmail.com>
2026-01-05 16:56:00 -0500

    dark mode and slider

---
+++ script.js
@@ -0,0 +1,29 @@
+// Dark mode 
+
+const themeToggle = document.getElementById('theme-toggle');
+const themeLabel = document.getElementById('theme-label');
+const body = document.body;
+
+const currentTheme = localStorage.getItem('theme') || 'dark';
+if (currentTheme === 'dark') {
+    body.classList.add('dark-mode');
+    themeToggle.checked = true;
+    themeLabel.textContent = 'dark mode';
+} else {
+    body.classList.remove('dark-mode');
+    themeToggle.checked = false;
+    themeLabel.textContent = 'light mode';
+}
+
+themeToggle.addEventListener('change', function() {
+    if (this.checked) {
+        body.classList.add('dark-mode');
+        localStorage.setItem('theme', 'dark');
+        themeLabel.textContent = 'dark mode';
+    } else {
+        body.classList.remove('dark-mode');
+        localStorage.setItem('theme', 'light');
+        themeLabel.textContent = 'light mode';
+    }
+});
+
